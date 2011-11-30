8c673625cc40a12c858c8286a576a1a08d6a30ad
Farruco Sanjurjo <farruco.sanjurjo@trabesoluciones.net>
2011-11-30 19:11:33 +0100

    Added minimal functionality

--- src/rebar_feature_runner.erl
+++ src/rebar_feature_runner.erl
@@ -2,5 +2,35 @@
 
 -export(['run-features'/2]).
 
+-define(DEBUG(Msg, Args), ?LOG(debug, Msg, Args)).
+-define(WARN(Msg, Args), ?LOG(warn, Msg, Args)).
+-define(LOG(Lvl, Msg, Args), rebar_log:log(Lvl, Msg, Args)).
+-define(ABORT(Msg, Args), rebar_utils:abort(Msg, Args)).
+
 'run-features'(RebarConfig, _Unknown) ->
-  ok.
+  ?DEBUG("Searching for features in ~s ~n", [rebar_utils:get_cwd()]),
+  FeaturesDir = filename:join(
+                  filename:join(rebar_utils:get_cwd(), rebar_config:get_global(path, "tests/"))
+                  , "features"),
+
+  case filelib:is_dir(FeaturesDir)
+  of
+    true ->
+      run_dir(FeaturesDir);
+    false ->
+      ok
+  end.
+
+run_dir(FeaturesDir) ->
+  {ok, Files} = file:list_dir(FeaturesDir),
+  run_features(lists:map(fun (File) -> filename:join(FeaturesDir, File) end, Files)).
+
+run_features([]) ->
+  ok;
+run_features([FeatureFile | Features]) ->
+  ?DEBUG("Running feature ~s ~n", [FeatureFile]),
+  run_feature(FeatureFile),
+  run_features(Features).
+
+run_feature(FeatureFile) ->
+  cucumberl:run(FeatureFile).
